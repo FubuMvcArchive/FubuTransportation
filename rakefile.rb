1f32c36181bc8c60c25a97fc026bb61658886318
jeremydmiller <jeremydmiller@yahoo.com>
2013-06-19 10:05:18 -0500

    how about some actual files?

---
+++ rakefile.rb
@@ -0,0 +1,25 @@
+begin
+  require 'bundler/setup'
+  require 'fuburake'
+rescue LoadError
+  puts 'Bundler and all the gems need to be installed prior to running this rake script. Installing...'
+  system("gem install bundler --source http://rubygems.org")
+  sh 'bundle install'
+  system("bundle exec rake", *ARGV)
+  exit 0
+end
+
+
+FubuRake::Solution.new do |sln|
+	sln.compile = {
+		:solutionfile => 'src/FubuTransportation.sln'
+	}
+				 
+	sln.assembly_info = {
+		:product_name => "FubuTransportation",
+		:copyright => 'Copyright 2013. All rights reserved.'
+	}
+	
+	sln.ripple_enabled = true
+	sln.fubudocs_enabled = true
+end
---
+++ src/FubuTransportation.Testing/StandinTester.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using FubuTestingSupport;
+
+namespace FubuTransportation.Testing
+{
+    [TestFixture]
+    public class StandinTester
+    {
+        [Test]
+        public void it_works()
+        {
+            2.ShouldEqual(2);
+        }
+    }
+}
