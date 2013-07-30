db5d6aebe36bfb7eb0c9a3b7615e5874c818e8e5
jeremydmiller <jeremydmiller@yahoo.com>
2013-07-30 08:37:09 -0500

    deterministic key for a channel

---
+++ src/FubuTransportation.Testing/Configuration/ChannelGraphTester.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FubuTransportation.Configuration;
+using NUnit.Framework;
+using FubuTestingSupport;
+
+namespace FubuTransportation.Testing.Configuration
+{
+    [TestFixture]
+    public class ChannelGraphTester
+    {
+        [Test]
+        public void to_key_by_expression()
+        {
+            ChannelGraph.ToKey<ChannelSettings>(x => x.Outbound)
+                        .ShouldEqual("Channel:Outbound");
+        }
+    }
+
+    public class ChannelSettings
+    {
+        public Uri Outbound { get; set; }
+    }
+}
--- src/FubuTransportation/Configuration/ChannelOptions.cs
+++ src/FubuTransportation/Configuration/ChannelOptions.cs
@@ -1,4 +1,6 @@
-﻿using FubuCore.Reflection;
+﻿using System;
+using System.Linq.Expressions;
+using FubuCore.Reflection;
 using FubuCore.Util;
 
 namespace FubuTransportation.Configuration
@@ -6,6 +8,16 @@ namespace FubuTransportation.Configuration
     public class ChannelGraph
     {
         private readonly Cache<string, ChannelOptions> _channels = new Cache<string, ChannelOptions>();
+
+        public static string ToKey(Accessor accessor)
+        {
+            return accessor.OwnerType.Name.Replace("Settings", "") + ":" + accessor.Name;
+        }
+
+        public static string ToKey<T>(Expression<Func<T, object>> property)
+        {
+            return ToKey(property.ToAccessor());
+        }
     }
 
     public class ChannelOptions
