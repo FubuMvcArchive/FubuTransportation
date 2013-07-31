1ea71cfd2d37f3e70c162c6d62575c4579812707
jeremydmiller <jeremydmiller@yahoo.com>
2013-07-31 09:53:37 -0500

    added simple routing rules for adhoc rule and single type

--- src/FubuTransportation/Runtime/Routing/IRoutingRule.cs
+++ src/FubuTransportation/Runtime/Routing/IRoutingRule.cs
@@ -6,4 +6,12 @@ namespace FubuTransportation.Runtime.Routing
     {
         bool Matches(Type type);
     }
+
+    public class SingleTypeRoutingRule<T> : IRoutingRule
+    {
+        public bool Matches(Type type)
+        {
+            return type == typeof (T);
+        }
+    }
 }
---
+++ src/FubuTransportation/Runtime/Routing/LambdaRoutingRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FubuTransportation.Runtime.Routing
+{
+    public class LambdaRoutingRule : IRoutingRule
+    {
+        private readonly Func<Type, bool> _filter;
+
+        public LambdaRoutingRule(Func<Type, bool> filter)
+        {
+            _filter = filter;
+        }
+
+        public bool Matches(Type type)
+        {
+            return _filter(type);
+        }
+    }
+}
