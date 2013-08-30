e739847636632623a4e5477d8cb1acc84e3783d5
jeremydmiller <jeremydmiller@yahoo.com>
2013-08-29 20:26:46 -0500

    added the ExceptionHandlerNode

---
+++ src/FubuTransportation/ErrorHandling/ExceptionHandlerNode.cs
@@ -0,0 +1,29 @@
+﻿using FubuMVC.Core.Registration.Nodes;
+using FubuMVC.Core.Registration.ObjectGraph;
+using FubuTransportation.Configuration;
+
+namespace FubuTransportation.ErrorHandling
+{
+    public class ExceptionHandlerNode : BehaviorNode
+    {
+        private readonly HandlerChain _chain;
+
+        public ExceptionHandlerNode(HandlerChain chain)
+        {
+            _chain = chain;
+        }
+
+        protected override ObjectDef buildObjectDef()
+        {
+            var def = ObjectDef.ForType<ExceptionHandlerBehavior>();
+            def.DependencyByValue(_chain);
+
+            return def;
+        }
+
+        public override BehaviorCategory Category
+        {
+            get { return BehaviorCategory.Wrapper; }
+        }
+    }
+}
