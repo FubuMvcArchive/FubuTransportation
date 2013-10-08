a3d8448508db45e24fdc7d1f35699653dc54b535
jeremydmiller <jeremydmiller@yahoo.com>
2013-10-08 08:20:42 -0500

    FubuTransport checks a property on PackageRegistry to know when to apply message watching.  Closes GH-100

---
+++ src/FubuTransportation.Testing/Configuration/FubuTransportTester.cs
@@ -0,0 +1,47 @@
+﻿using Bottles;
+using FubuTestingSupport;
+using FubuTransportation.Configuration;
+using NUnit.Framework;
+
+namespace FubuTransportation.Testing.Configuration
+{
+    [TestFixture]
+    public class FubuTransportTester
+    {
+        [Test]
+        public void if_package_registry_ft_testing_is_true()
+        {
+            FubuTransport.Reset();
+            
+            PackageRegistry.Properties[FubuTransport.FT_TESTING] = "true";
+
+            FubuTransport.ApplyMessageHistoryWatching.ShouldBeTrue();
+            FubuTransport.AllQueuesInMemory.ShouldBeFalse(); // Don't want this at all
+            FubuTransport.UseSynchronousLogging.ShouldBeTrue();
+        }
+
+        [Test]
+        public void if_package_registry_ft_testing_is_missing()
+        {
+            FubuTransport.Reset();
+
+            PackageRegistry.Properties.Remove(FubuTransport.FT_TESTING);
+
+            FubuTransport.ApplyMessageHistoryWatching.ShouldBeFalse();
+            FubuTransport.AllQueuesInMemory.ShouldBeFalse(); // Don't want this at all
+            FubuTransport.UseSynchronousLogging.ShouldBeFalse();
+        }
+
+        [Test]
+        public void if_package_registry_ft_testing_is_false()
+        {
+            FubuTransport.Reset();
+
+            PackageRegistry.Properties[FubuTransport.FT_TESTING] = "false";
+
+            FubuTransport.ApplyMessageHistoryWatching.ShouldBeFalse();
+            FubuTransport.AllQueuesInMemory.ShouldBeFalse(); // Don't want this at all
+            FubuTransport.UseSynchronousLogging.ShouldBeFalse();
+        }
+    }
+}
--- src/FubuTransportation/Configuration/FubuTransport.cs
+++ src/FubuTransportation/Configuration/FubuTransport.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Bottles;
 using FubuMVC.Core;
 using FubuCore;
 using FubuMVC.Core.Registration.DSL;
@@ -11,6 +12,11 @@ namespace FubuTransportation.Configuration
     /// </summary>
     public static class FubuTransport
     {
+        public static readonly string FT_TESTING = "FubuTransportTesting";
+
+        private static bool _useSynchronousLogging;
+        private static bool _applyMessageHistoryWatching;
+
         public static IContainerFacilityExpression For<T>() where T : FubuTransportRegistry, new()
         {
             var extension = new T();
@@ -47,8 +53,36 @@ namespace FubuTransportation.Configuration
             UseSynchronousLogging = ApplyMessageHistoryWatching = AllQueuesInMemory = false;
         }
 
-        public static bool UseSynchronousLogging { get; set; }
-        public static bool ApplyMessageHistoryWatching { get; set; }
+        public static bool UseSynchronousLogging
+        {
+            get
+            {
+                bool returnValue = false;
+                if (bool.TryParse(PackageRegistry.Properties[FT_TESTING], out returnValue))
+                {
+                    return returnValue;
+                }
+
+                return _useSynchronousLogging;
+            }
+            set { _useSynchronousLogging = value; }
+        }
+
+        public static bool ApplyMessageHistoryWatching
+        {
+            get
+            {
+                bool returnValue = false;
+                if (bool.TryParse(PackageRegistry.Properties[FT_TESTING], out returnValue))
+                {
+                    return returnValue;
+                }
+                
+                return _applyMessageHistoryWatching;
+            }
+            set { _applyMessageHistoryWatching = value; }
+        }
+
         public static bool AllQueuesInMemory { get; set; }
 
         public static void SetupForInMemoryTesting()
