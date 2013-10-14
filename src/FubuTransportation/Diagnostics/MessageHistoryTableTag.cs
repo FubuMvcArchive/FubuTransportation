c5fdf62357d6950e986ae5da79a78d86caab6651
jeremydmiller <jeremydmiller@yahoo.com>
2013-10-14 09:47:18 -0500

    lot of work to pretty up the message history html visualization

--- src/FubuTransportation/Diagnostics/MessageHistoryTableTag.cs
+++ src/FubuTransportation/Diagnostics/MessageHistoryTableTag.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using HtmlTags;
 
 namespace FubuTransportation.Diagnostics
@@ -7,34 +8,65 @@ namespace FubuTransportation.Diagnostics
     {
         public MessageHistoryTableTag(MessageHistory history)
         {
-            AddHeaderRow(tr => { tr.Header().Attr("colspan", "4"); });
+            Style("width", "inherit");
+            Style("margin-top", "50px");
 
             AddHeaderRow(tr => {
-                tr.Header("Node");
-                tr.Header("Timestamp");
-                tr.Header("Message");
-                tr.Header("Headers");
+                tr.Header(history.Description).Attr("colspan", "5").Style("text-align", "left");
             });
 
+            AddHeaderRow(tr => {
+                tr.Header("Node").Style("text-align", "left");
+                tr.Header("Timestamp").Style("text-align", "left");
+                tr.Header("Message").Style("text-align", "left").Style("width", "200px");
+                tr.Header("Header = ").Style("text-align", "right");
+                tr.Header("Value").Style("text-align", "left");
+            });
+
+           
+
             history.Records().Each(rec => {
+                var headers = rec.Headers.Split(';').ToArray();
+                
+
                 AddBodyRow(tr => {
-                    tr.Cell(rec.Node);
-                    tr.Cell(rec.Timestamp.ToLongTimeString());
-                    tr.Cell(rec.Message);
-                    tr.Cell(rec.Headers);
+                    tr.Cell(rec.Node).Attr("valign", "top").Style("padding-right", "30px");
+                    tr.Cell(rec.Timestamp.ToLongTimeString()).Style("padding-right", "30px").Attr("valign", "top").Attr("nowrap", "true");
+                    tr.Cell(rec.Message).Attr("valign", "top").Style("padding-right", "30px");
+                    
+                    if (headers.Any())
+                    {
+                        var count = headers.Count().ToString();
+                        tr.Children.Each(x => x.Attr("rowspan", count));
+
+                        string headerValue = headers.First();
+                        writeHeaderValue(headerValue, tr);
+                    }
                 });
+
+                for (int i = 1; i < headers.Count(); i++)
+                {
+                    AddBodyRow(tr => writeHeaderValue(headers[i], tr));
+                }
 
                 if (FubuCore.StringExtensions.IsNotEmpty(rec.ExceptionText))
                 {
                     AddBodyRow(tr => {
                         var cell = tr.Cell();
-                        cell.Attr("colspan", "4");
+                        cell.Attr("colspan", "5");
 
 
                         cell.Add("pre").Text(rec.ExceptionText).Style("background-color", "#FFFFAA");
                     });
                 }
             });
+        }
+
+        private static void writeHeaderValue(string headerValue, TableRowTag tr)
+        {
+            var parts = headerValue.Split('=');
+            tr.Cell(parts.First() + " = ").Style("text-align", "right");
+            tr.Cell(parts.Last());
         }
     }
 }
