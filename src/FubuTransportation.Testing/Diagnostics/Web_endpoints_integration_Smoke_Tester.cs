f8a5f144752de09602d179af6326ab3e1ec759ba
jeremydmiller <jeremydmiller@yahoo.com>
2013-11-19 15:55:10 -0600

    adding an integration test for one of the diagnostic screens

---
+++ src/FubuTransportation.Testing/Diagnostics/Web_endpoints_integration_Smoke_Tester.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using FubuMVC.Core;
+using FubuMVC.Core.Registration;
+using FubuMVC.Katana;
+using FubuMVC.StructureMap;
+using FubuTestingSupport;
+using FubuTransportation.Configuration;
+using FubuTransportation.Diagnostics.Visualization;
+using NUnit.Framework;
+
+namespace FubuTransportation.Testing.Diagnostics
+{
+    [TestFixture]
+    public class Web_endpoints_integration_Smoke_Tester
+    {
+        [Test]
+        public void the_message_handlers_visualization_can_be_shown()
+        {
+            using (var server = EmbeddedFubuMvcServer.For<DiagnosticApplication>())
+            {
+                server.Endpoints.Get<MessagesFubuDiagnostics>(x => x.get_messages())
+                    .StatusCode.ShouldEqual(HttpStatusCode.OK);
+            }
+        }
+    }
+
+    public class DiagnosticApplication : FubuTransportRegistry, IApplicationSource
+    {
+        public DiagnosticApplication()
+        {
+            EnableInMemoryTransport();
+        }
+
+        public FubuApplication BuildApplication()
+        {
+            var registry = new FubuRegistry();
+            registry.Import<DiagnosticApplication>();
+
+            return FubuApplication.For(registry).StructureMap();
+        }
+    }
+}
