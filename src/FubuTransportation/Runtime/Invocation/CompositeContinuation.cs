8964a009f4d0df69d91a85321fcaf949308ab806
jeremydmiller <jeremydmiller@yahoo.com>
2013-12-18 15:38:16 -0600

    added the CompositeContinuation

---
+++ src/FubuTransportation/Runtime/Invocation/CompositeContinuation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace FubuTransportation.Runtime.Invocation
+{
+    public class CompositeContinuation : IContinuation, IEnumerable<IContinuation>
+    {
+        private readonly IList<IContinuation> _continuations = new List<IContinuation>();
+
+        public CompositeContinuation(params IContinuation[] continuations)
+        {
+            _continuations.AddRange(continuations);
+        }
+
+        public void Execute(Envelope envelope, ContinuationContext context)
+        {
+            _continuations.Each(x => x.Execute(envelope, context));
+        }
+
+        public void Add(IContinuation child)
+        {
+            _continuations.Add(child);
+        }
+
+
+        public IEnumerator<IContinuation> GetEnumerator()
+        {
+            return _continuations.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
