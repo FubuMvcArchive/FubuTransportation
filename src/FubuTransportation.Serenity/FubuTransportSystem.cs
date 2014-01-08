e66287206ddcacf26803e003bc83850b65d622d2
jeremydmiller <jeremydmiller@yahoo.com>
2014-01-08 14:21:08 -0600

    removing the duplication in the FubuTransportSystem ctor

--- src/FubuTransportation.Serenity/FubuTransportSystem.cs
+++ src/FubuTransportation.Serenity/FubuTransportSystem.cs
@@ -12,27 +12,16 @@ namespace FubuTransportation.Serenity
 {
     public class FubuTransportSystem<T> : FubuMvcSystem<T> where T : IApplicationSource, new()
     {
-        public FubuTransportSystem()
+        public FubuTransportSystem() : this(DetermineSettings())
         {
-            FubuTransport.SetupForTesting(); // Uses FubuMode.SetUpTestingMode();
 
-            AddContextualProvider<MessageContextualInfoProvider>();
-
-            OnStartup<IMessagingSession>(x => {
-                Bottles.Services.Messaging.EventAggregator.Messaging.AddListener(x);
-            });
-
-            // Clean up all the existing queue state to prevent test pollution
-            OnContextCreation<TransportCleanup>(cleanup => {
-                cleanup.ClearAll();
-
-                RemoteSubSystems.Each(x => x.Runner.SendRemotely(new ClearAllTransports()));
-            });
-
-            OnContextCreation<IMessagingSession>(x => {
-                RemoteSubSystems.Each(sys => sys.Runner.Messaging.AddListener(x));
-            });
         }
+
+        public FubuTransportSystem(string parallelDirectory = null, string physicalPath = null)
+            : this(DetermineSettings(parallelDirectory, physicalPath))
+        {
+            
+        } 
 
         public FubuTransportSystem(ApplicationSettings settings) : base(settings)
         {
