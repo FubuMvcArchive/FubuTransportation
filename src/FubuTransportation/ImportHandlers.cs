effda9371d447c566d2f195dba9281b364ec60e1
jeremydmiller <jeremydmiller@yahoo.com>
2014-05-15 22:20:01 -0500

    some new perf diagnostics

--- src/FubuTransportation/ImportHandlers.cs
+++ src/FubuTransportation/ImportHandlers.cs
@@ -40,7 +40,7 @@ namespace FubuTransportation
 
                 // Hate how we're doing this, but disable tracing
                 // on the polling job requests here.
-                if (chain.InputType().Closes(typeof(JobRequest<>)))
+                if (chain.InputType().Closes(typeof (JobRequest<>)))
                 {
                     chain.Tags.Add(BehaviorChain.NoTracing);
                 }
@@ -48,6 +48,5 @@ namespace FubuTransportation
 
             return handlers;
         }
-
     }
 }
