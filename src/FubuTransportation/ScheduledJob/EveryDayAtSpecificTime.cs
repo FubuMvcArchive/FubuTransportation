e8cd9f45147879bd5a3bd8ec2a7251dad32c080a
mkmurray <charliebrown928@hotmail.com>
2014-08-08 09:46:55 -0600

    Daily job scheduler base implementation

---
+++ src/FubuTransportation/ScheduledJob/EveryDayAtSpecificTime.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace FubuTransportation.ScheduledJob
+{
+    public class EveryDayAtSpecificTime : IJobScheduler
+    {
+        private readonly int _hour;
+        private readonly int _minute;
+
+        public EveryDayAtSpecificTime(int hour, int minute)
+        {
+            _hour = hour;
+            _minute = minute;
+        }
+
+        public DateTimeOffset ScheduleNextTime(DateTimeOffset currentTime)
+        {
+            var localTime = currentTime.ToLocalTime();
+            var oneAmToday = new DateTime(localTime.Year, localTime.Month, localTime.Day, _hour, _minute, 0, 0, DateTimeKind.Local);
+            var nextScheduledTime = oneAmToday;
+
+            if (localTime.Hour > _hour || (localTime.Hour == _hour && localTime.Minute >= _minute))
+            {
+                // Switch to tomorrow
+                nextScheduledTime = oneAmToday.AddDays(1);
+            }
+
+            return nextScheduledTime.ToUniversalTime();
+        }
+    }
+
+    /*
+    public class EveryDayAt1Am : EveryDayAtSpecificTime
+    {
+        public EveryDayAt1Am() : base(hour: 01, minute: 00)
+        {}
+    }
+    */
+}
