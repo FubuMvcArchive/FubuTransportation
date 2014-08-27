6d2c6f56be20a6b4c5180cc58759e49a291496a5
jeremydmiller <jeremydmiller@yahoo.com>
2014-08-27 02:10:18 -0500

    big test harness for PersistentTaskController, can find a task now

--- src/FubuTransportation/Monitoring/PersistentTaskController.cs
+++ src/FubuTransportation/Monitoring/PersistentTaskController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FubuCore.Logging;
 using FubuCore.Util;
 using FubuTransportation.Subscriptions;
 
@@ -11,6 +12,7 @@ namespace FubuTransportation.Monitoring
     // Use an agent for each subject that uses a producer/consumer task
     public class PersistentTaskController
     {
+        private readonly ILogger _logger;
         private readonly ITransportPeerRepository _repository;
         private readonly ConcurrentCache<string, IPersistentTaskSource> _sources 
             = new ConcurrentCache<string, IPersistentTaskSource>(); 
@@ -19,11 +21,12 @@ namespace FubuTransportation.Monitoring
             new ConcurrentCache<Uri, PersistentTaskAgent>();
 
 
-        private Uri[] _permanentTasks;
+        private readonly Uri[] _permanentTasks;
 
 
-        public PersistentTaskController(ITransportPeerRepository repository, IEnumerable<IPersistentTaskSource> sources)
+        public PersistentTaskController(ILogger logger, ITransportPeerRepository repository, IEnumerable<IPersistentTaskSource> sources)
         {
+            _logger = logger;
             _repository = repository;
             sources.Each(x => _sources[x.Protocol] = x);
 
@@ -35,23 +38,29 @@ namespace FubuTransportation.Monitoring
 
         public IPersistentTask FindTask(Uri subject)
         {
+            if (!_sources.Has(subject.Scheme)) return null;
+
             var source = _sources[subject.Scheme];
             if (source == null) return null;
 
             return source.CreateTask(subject);
 
-            throw new NotImplementedException();
         }
 
         public void ActivateAllTasks()
         {
-            var startupTasks = _permanentTasks.Select(uri => {
-                return _agents[uri].Enqueue(t => t.Activate());
-            }).ToArray();
+
+            throw new NotImplementedException();
+
+            var startupTasks = _permanentTasks.Select(uri => _agents[uri].Activate()).ToArray();
+
+            Task.WaitAll(startupTasks);
         }
 
         public void EnsureTasksHaveOwnership()
         {
+
+
             // check the health of all owned tasks
 
             // hit all owners 
@@ -73,14 +82,53 @@ namespace FubuTransportation.Monitoring
 
         public Task<OwnershipStatus> TakeOwnership(Uri subject)
         {
+            var agent = _agents[subject];
+            if (agent == null)
+            {
+                return OwnershipStatus.UnknownSubject.ToCompletionTask();
+            }
+            if (agent.IsActive())
+            {
+                return OwnershipStatus.AlreadyOwned.ToCompletionTask();
+            }
+            
+            
+            return agent.Activate().ContinueWith(t => {
+                if (t.IsFaulted)
+                {
+                    // TODO -- log
+                    return OwnershipStatus.Exception;
+                }
 
-
-
-            throw new NotImplementedException();
+                // TODO -- persist the ownership
+                return OwnershipStatus.OwnershipActivated;
+            });
         }
 
         public Task<HealthStatus> CheckStatus(Uri subject)
         {
+            var agent = _agents[subject];
+
+            if (agent == null)
+            {
+                return HealthStatus.Unknown.ToCompletionTask();
+            }
+
+            if (agent.IsActive())
+            {
+                return agent.AssertAvailable().ContinueWith(t => {
+                    if (t.IsFaulted)
+                    {
+                        // TODO -- log
+                        return HealthStatus.Error;
+                    }
+
+                    return HealthStatus.Active;
+                });
+            }
+            
+            return HealthStatus.Inactive.ToCompletionTask();
+
             // active, inactive, error, unknown
 
             throw new NotImplementedException();
---
+++ src/FubuTransportation/TaskExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace FubuTransportation
+{
+    public static class TaskExtensions
+    {
+        public static Task<T> ToCompletionTask<T>(this T value)
+        {
+            var completion = new TaskCompletionSource<T>();
+            completion.SetResult(value);
+
+            return completion.Task;
+        } 
+    }
+}
