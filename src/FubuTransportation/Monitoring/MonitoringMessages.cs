92d14d456b554bff07f22aa49593c70e3378b28e
jeremydmiller <jeremydmiller@yahoo.com>
2014-08-26 20:57:20 -0500

    a lot of spiking for monitoring

---
+++ src/FubuTransportation/Monitoring/MonitoringMessages.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace FubuTransportation.Monitoring
+{
+    public class TaskHealthRequest
+    {
+        public Uri[] Subjects { get; set; }
+    }
+
+    public class TaskHealthResponse
+    {
+        public TaskStatus[] Tasks { get; set; }
+    }
+
+    public class TaskStatus
+    {
+        public Uri Subject { get; set; }
+        public HealthStatus Status { get; set; }
+    }
+
+    public enum HealthStatus
+    {
+        Active,
+        Unknown,
+        Error,
+        Inactive
+    }
+
+    public class TakeOwnershipRequest
+    {
+        public Uri Subject { get; set; }
+    }
+
+    public class TakeOwnershipResponse
+    {
+        public Uri Subject { get; set; }
+        public OwnershipStatus Status { get; set; }
+        public string NodeId { get; set; }
+    }
+
+    public enum OwnershipStatus
+    {
+        Activated,
+        Exception,
+        AlreadyOwned,
+        UnknownSubject
+    }
+
+}
