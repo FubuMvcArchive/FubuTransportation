a17a92e8dbc271f9d3e1e70c8307f58ab640c7f8
jeremydmiller <jeremydmiller@yahoo.com>
2014-08-30 06:41:50 -0500

    added the polling job for health monitoring

--- src/FubuTransportation/Polling/BuiltInPollingJobRegistry.cs
+++ src/FubuTransportation/Polling/BuiltInPollingJobRegistry.cs
@@ -1,19 +1,23 @@
 ﻿using FubuTransportation.Configuration;
 using FubuTransportation.Events;
+using FubuTransportation.Monitoring;
 using FubuTransportation.Runtime.Delayed;
 
 namespace FubuTransportation.Polling
 {
     public class BuiltInPollingJobRegistry : FubuTransportRegistry
     {
-        public BuiltInPollingJobRegistry() 
+        public BuiltInPollingJobRegistry()
         {
             Handlers.DisableDefaultHandlerSource();
             Polling.RunJob<DelayedEnvelopeProcessor>()
-                   .ScheduledAtInterval<TransportSettings>(x => x.DelayMessagePolling);
+                .ScheduledAtInterval<TransportSettings>(x => x.DelayMessagePolling);
 
             Polling.RunJob<ExpiringListenerCleanup>()
                 .ScheduledAtInterval<TransportSettings>(x => x.ListenerCleanupPolling);
+
+            Polling.RunJob<HealthMonitorPollingJob>()
+                .ScheduledAtInterval<HealthMonitoringSettings>(x => x.Interval);
         }
     }
 }
