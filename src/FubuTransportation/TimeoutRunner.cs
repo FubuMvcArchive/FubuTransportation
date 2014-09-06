78bbd66ae3f5ac6ece1d9031a32ec7f63de7631b
jeremydmiller <jeremydmiller@yahoo.com>
2014-09-06 05:48:36 -0500

    added the TimeoutRunner

---
+++ src/FubuTransportation/TimeoutRunner.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace FubuTransportation
+{
+    public enum Completion
+    {
+        Success,
+        Timedout,
+        Exception
+    }
+
+    public static class TimeoutRunner
+    {
+        public static Completion Run(TimeSpan timeout, Action action, Action<Exception> onError)
+        {
+            var returnValue = Completion.Timedout;
+
+            var reset = new ManualResetEvent(false);
+
+            var thread = new Thread(() => {
+                try
+                {
+                    action();
+                    reset.Set();
+                    returnValue = Completion.Success;
+                }
+                catch (ThreadAbortException e)
+                {
+                    // nothing
+                }
+                catch (Exception ex)
+                {
+                    returnValue = Completion.Exception;
+                    onError(ex);
+                    reset.Set();
+                }
+            });
+
+            thread.Start();
+            if (!reset.WaitOne(timeout))
+            {
+                thread.Abort();
+                returnValue = Completion.Timedout;
+            }
+
+            return returnValue;
+        }
+
+        
+    }
+}
