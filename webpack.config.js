dd0a2c19449c68a96edfbaa493eea5bc8ab5e3a4
Ryan Hauert <ryanoffski@gmail.com>
2015-04-02 10:21:43 -0600

    Adding diagnostics dependencies

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+
+module.exports = {
+  entry: {
+    "fubu-transportation": './javascript/fubu-transportation.jsx'
+  },
+  output: {
+    //path: __dirname + '/src/DiagnosticsHarness/fubu-diagnostics',
+    path: __dirname + '/src/FubuTransporation/fubu-diagnostics',
+    filename: "[name].js",
+    publicPath: '/client/public/javascript/',
+    pathinfo: true
+  },
+  resolve: {
+    // Allow to omit extensions when requiring these files
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.jsx$/, loader: 'jsx?harmony'},
+      { test: /\.js$/, exclude: /(node_modules)|(-data\.js$)/, loader: "babel"}
+    ]
+  },
+  externals: {
+      // require("jquery") is external and available
+      //  on the global var jQuery
+      "jquery": "jQuery"
+  },
+
+  devtool: 'eval'
+}
